99c1749f868de934c783921c710123dbc21351cb
Nicolás Passerini <npasserini@gmail.com>
2015-11-08 23:06:47 -0300

    Fix test grammar

--- 00100_Creando a Pepita/test.js
+++
@@ -1,8 +0,0 @@
-describe("pepita", function(){
-  it( "existe", function(){
-    assert(typeof(pepita) !== undefined );
-  });
-  it("es un objecto", function(){
-    assert(pepita.constructor == Object);
-  });
-})
